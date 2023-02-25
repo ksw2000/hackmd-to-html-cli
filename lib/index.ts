ec19eb9e164a3d5b8e54a52d56eb55d0d5f88b5e
liao2000 <liao2000@livemail.tw>
2023-02-25 21:11:17 +0800

    Prepare v0.0.8

--- lib/index.ts
+++ lib/index.ts
@@ -3,7 +3,7 @@ import commander from 'commander'
 import path from 'path'
 import { Convert } from './converter'
 
-commander.program.version('0.0.7', '-v, --version', 'output the current version')
+commander.program.version('0.0.8', '-v, --version', 'output the current version')
 commander.program
   .requiredOption('-s, --source <files_or_dirs...>', 'specify the input markdown files or directories')
   .addOption(new commander.Option('-d, --destination <path>', 'specify the output directory').default('./output', './output'))
