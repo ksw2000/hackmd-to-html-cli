cac5db43c3618c9c16bdb54afe6dbf7e48e408e5
Kashiwa <liao2000@livemail.tw>
2023-06-17 14:33:40 +0800

    --src instead of --source

--- lib/cli.ts
+++ lib/cli.ts
@@ -5,18 +5,18 @@ import { Converter } from './converter'
 
 commander.program.version('0.0.12', '-v, --version', 'output the current version')
 commander.program
-  .requiredOption('-s, --source <files_or_dirs...>', 'specify the input markdown files or directories')
-  .addOption(new commander.Option('-d, --destination <path>', 'specify the output directory').default('./output', './output'))
-  .addOption(new commander.Option('-l, --layout <html_file>', 'specify the layout file').default('', '""'))
-  .addOption(new commander.Option('-b, --hardBreak', 'use hard break instead of soft break'))
-  .parse(process.argv)
+    .requiredOption('-s, --src <files_or_dirs...>', 'specify the input markdown files or directories')
+    .addOption(new commander.Option('-d, --dest <path>', 'specify the output directory').default('./output', './output'))
+    .addOption(new commander.Option('-l, --layout <html_file>', 'specify the layout file').default('', '""'))
+    .addOption(new commander.Option('-b, --hardBreak', 'use hard break instead of soft break'))
+    .parse(process.argv)
 
 const options = commander.program.opts()
 
-const dest: string = options.destination === '' ? './output' : options.destination
+const dest: string = options.dest === '' ? './output' : options.dest
 const layout: string | null = options.layout !== '' ? fs.readFileSync(options.layout, { encoding: 'utf-8' }) : null
 const hardBreak: boolean = options.hardBreak
 
 const converter = new Converter(layout, hardBreak)
 
-converter.convertFiles(options.source, dest)
+converter.convertFiles(options.src, dest)
