db7eb020e46be41c490e4cb655c2f788d5900172
JeffBla <jeff920305@gmail.com>
2025-04-02 10:46:13 +0800

    feat: support HackMD extraction with HTML unescape
    Introduce a -m, --hmd option to properly handle url to markdown from
    HackMD pages. Extract content from <div id="publish-page"> to avoid
    processing unnecessary HTML. Add reverseEscapeHtml in utils.ts to
    unescape pre-escaped characters in the fetched content, preventing
    double escaping when rendered. These changes improve compatibility with
    HackMD and ensure correctly formatted output.

--- src/markdown/utils.ts
+++ src/markdown/utils.ts
@@ -3,6 +3,8 @@
 
 const HTML_ESCAPE_TEST_RE = /[&<>"]/
 const HTML_ESCAPE_REPLACE_RE = /[&<>"]/g
+const HTML_UNESCAPE_TEST_RE = /&(?:amp|lt|gt|quot|#39|#34);/;
+const HTML_UNESCAPE_REPLACE_RE = /&(?:amp|lt|gt|quot|#39|#34);/g;
 
 function replaceUnsafeChar(ch: string): string {
     switch (ch) {
@@ -18,9 +20,35 @@ function replaceUnsafeChar(ch: string): string {
     return ""
 }
 
+
+function replaceEscapedChar(entity: string): string {
+    switch (entity) {
+      case "&amp;":
+        return "&";
+      case "&lt;":
+        return "<";
+      case "&gt;":
+        return ">";
+      case "&quot;":
+        return '"';
+      case "&#39;":
+        return "'";
+      case "&#34;":
+        return '"';
+    }
+    return "";
+  }
+
 export function escapeHtml(str: string): string {
     if (HTML_ESCAPE_TEST_RE.test(str)) {
         return str.replace(HTML_ESCAPE_REPLACE_RE, replaceUnsafeChar)
     }
     return str
 }
+
+export function reverseEscapeHtml(str: string): string {
+    if (HTML_UNESCAPE_TEST_RE.test(str)) {
+      return str.replace(HTML_UNESCAPE_REPLACE_RE, replaceEscapedChar);
+    }
+    return str;
+  }  
